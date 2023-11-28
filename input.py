4c3e5174916b0663b26448925167943076fcfab0
AakibAlam <aakibalam20@iitk.ac.in>
2023-11-28 18:27:01 +0530

    Hide confidential information

--- input.py
+++ input.py
@@ -1,4 +1,4 @@
 yoursEmail = 'aakibalam0808@gmail.com'
 friendsList = ['PRO_fessor_', 'Mayank_Pushpjeet', 'udhavvarma03', 'satyam343', 'sultan__', 'onetap']
-my_emailId = 'tamepass00@gmail.com'
-password = 'aihfsywcvhdbablr'
+my_emailId = 'enter your gmail here'
+password = 'enter your password here'
