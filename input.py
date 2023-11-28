7bd38a223b0506496a42626671e73d514cc11e04
AakibAlam <aakibalam20@iitk.ac.in>
2023-11-28 18:08:26 +0530

    Don't Know

---
+++ input.py
@@ -0,0 +1,4 @@
+yoursEmail = 'aakibalam0808@gmail.com'
+friendsList = ['PRO_fessor_', 'Mayank_Pushpjeet', 'udhavvarma03', 'satyam343', 'sultan__', 'onetap']
+my_emailId = 'tamepass00@gmail.com'
+password = 'aihfsywcvhdbablr'
